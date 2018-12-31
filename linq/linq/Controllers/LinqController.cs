c7e7cd79387f781c775477e36a24409b1b21ff8a
Deng-You-Jie <dicka1262061@gmail.com>
2018-12-31 13:22:58 +0800

    1 edit

---
+++ linq/linq/Controllers/LinqController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using linq.Models;
+
+namespace linq.Controllers
+{
+    public class LinqController : Controller
+    {
+        // GET: Linq
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public string ShowEmployee()
+        {
+            NorthwindEntities db = new NorthwindEntities();
+            var reslut = db.員工;
+            var show = "";
+            foreach(var item in reslut)
+            {
+
+                show += "編號:" + item.員工編號 + "<br/>" + "姓名:" + item.姓名 + "<br/>" + "職稱" + item.職稱 + "<hr/>";
+            }
+            return show;
+
+        }
+    }
+}
