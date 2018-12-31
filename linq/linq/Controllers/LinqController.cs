61c39beec89c8b3da3db54513ae80993c82f0b9b
Deng-You-Jie <dicka1262061@gmail.com>
2018-12-31 13:30:07 +0800

    2 edit

--- linq/linq/Controllers/LinqController.cs
+++ linq/linq/Controllers/LinqController.cs
@@ -27,5 +27,18 @@ namespace linq.Controllers
             return show;
 
         }
+        public string ShowCustomByAddress(string keyword)
+        {
+            NorthwindEntities db = new NorthwindEntities();
+            var reslut = db.客戶.Where(m=>m.地址.Contains(keyword);
+            var show = "";
+            foreach (var item in reslut)
+            {
+
+                show += "公司:" + item.公司名稱 + "<br/>" + "姓名:" + item.連絡人 + "<br/>" + "地址" + item.地址 + "<hr/>";
+            }
+            return show;
+
+        }
     }
 }
