284a7c4427e65046527061e1b801c581d5f7d1ec
Deng-You-Jie <dicka1262061@gmail.com>
2018-12-31 23:12:02 +0800

    1 edit

---
+++ slnModelValidate/slnModelValidate/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using slnModelValidate.Models;
+
+
+namespace slnModelValidate.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+        dbStudentEntities db = new dbStudentEntities();
+        // GET: Home
+        public ActionResult Index()
+        {
+            var student = db.tStudent.ToList();
+            return View(student);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(tStudent stu)
+        {
+            db.tStudent.Add(stu);
+            db.SaveChanges();
+            return RedirectToAction("index");
+        }
+        public ActionResult Delete(string id)
+        {
+            var stu = db.tStudent
+                .Where(m => m.fStuId == id).FirstOrDefault();
+            db.tStudent.Remove(stu);
+            db.SaveChanges();
+            return RedirectToAction("index");
+        }
+    }
+}
